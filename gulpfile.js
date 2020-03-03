0026737f91573d9a5d71f2b4068fd60e7243494f
zabit-musalaev <zabit.mv@gmail.com>
2020-03-03 16:01:33 +0300

    include css-min

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 const gulp = require('gulp');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
 
 gulp.task('hello', function(done) {
@@ -14,4 +16,12 @@ gulp.task('browser-sync', function() {
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('cssmin', function (done) {
+  gulp.src('css/*.css')
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('dist'));
+      done();
 });
